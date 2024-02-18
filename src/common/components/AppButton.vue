a2e6d187f694b90c983a1a7360b21cdd298381a4
dmitry.perepelkin <dmitry.perepelkin@kepler.team>
2024-02-19 00:16:53 +0300

    Added AppButton

---
+++ src/common/components/AppButton.vue
@@ -0,0 +1,53 @@
+<template>
+  <button class="button button--primary">Submit</button>
+
+</template>
+
+<script lang="ts">
+import {defineComponent, PropType} from 'vue';
+
+
+export default defineComponent({
+  name: 'AppButton',
+  props: {
+    type: {
+      type: String as PropType <'primary' | 'secondary'>,
+      default: ''
+    },
+    textSize: {
+      type: String as PropType <'largeText'>,
+      default: ''
+    }
+  }
+});
+</script>
+
+<style scoped lang="scss">
+/* Блок "button" */
+.button {
+  display: inline-block;
+  padding: 10px 20px;
+  font-size: 16px;
+  border: 2px solid transparent;
+  border-radius: 5px;
+  background-color: #007bff;
+  color: #fff;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+/* Модификатор "button primary" */
+.primary {
+  background-color: #007bff;
+}
+
+/* Модификатор "button secondary" */
+.secondary {
+  background-color: #6c757d;
+}
+
+/* Элемент "button text" */
+.largeText {
+  font-size: 24px
+}
+</style>
